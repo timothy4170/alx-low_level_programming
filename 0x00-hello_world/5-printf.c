a53287225abbf7767fe0cc4c38845624bf3ede8c
timothy4170 <bushtimtimothy@gmail.com>
2023-02-18 23:46:14 +0300

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main -print a c program using printif
+ *
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer. but the outcome is a piece of art,\n");
+	return (0);
+}
