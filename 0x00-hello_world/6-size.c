96a14552136100ecb8de08b4a0c111e471a6cefe
timothy4170 <bushtimtimothy@gmail.com>
2023-02-19 00:22:45 +0300

    6-size-c'
    exit
    
    git commit -m 6-size-c
    
    exit
    
    git commit -m "6-size-c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,27 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main -print out size data types in C
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long d;
+	float f;
+
+	printf("Size of char: %lu byte(S)\n" , (unsigned long)sizeof(a));
+	printf("Size of int: %lu byte(S)\n" , (unsigned long)sizeof(b));
+
+	("Size of long int: %lu byte(S)\n" , (unsigned long)sizeof(c));
+
+	("Size of long long int: %lu byte(S)\n" , (unsigned long)sizeof(d));
+
+	("Size of a float: %lu byte(S)\n" , (unsigned long)sizeof(f);
+
+
+	return (0);
+
+}
 
