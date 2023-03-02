8bfbd95e276dd1dec38a9bd92ad08e523410de24
timothy4170 <bushtimtimothy@gmail.com>
2023-03-02 04:10:56 -0800

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * 0_strcat cocatenates two strings
+ * @ dest: input value
+ * @src: input value
+ *
+ * return: void
+
+*/ char *_strcat (char * dest, char *src)
+{
+	int i
+		int j
+		i=0
+		while (dest[i])
+		{
+			i++
+		}
+	j=0
+		while (scr[j] !=0)
+		{
+			dest[i]=src[j]
+				j++
+		}
+				dest [i]=0
+				return(dest)
+}
